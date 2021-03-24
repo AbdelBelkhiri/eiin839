3da6ec507ecb7589f53a8bb0c47d643d5d020613
Abdel <abdelouhab.belkhiri@etu.unice.fr>
2021-03-24 14:39:16 +0100

    add TD5 (session 4)

---
+++ MathsOperations/MathsLibrary/IMathsOperations.cs
@@ -0,0 +1,55 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace MathsLibrary
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom d'interface "IService1" à la fois dans le code et le fichier de configuration.
+    [ServiceContract]
+    public interface IMathsOperations
+    {
+        [OperationContract]
+        [WebInvoke(UriTemplate = "add?v1={v1}&v2={v2}", Method = "GET", BodyStyle = WebMessageBodyStyle.Wrapped, ResponseFormat = WebMessageFormat.Json)]
+        float add(int v1, int v2);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "substract?v1={v1}&v2={v2}", Method = "GET", BodyStyle = WebMessageBodyStyle.Wrapped, ResponseFormat = WebMessageFormat.Json)]
+        float substract(int v1, int v2);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "multiply?v1={v1}&v2={v2}", Method = "GET", BodyStyle = WebMessageBodyStyle.Wrapped, ResponseFormat = WebMessageFormat.Json)]
+        float multiply(int v1, int v2);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "divide?v1={v1}&v2={v2}", Method = "GET", BodyStyle = WebMessageBodyStyle.Wrapped, ResponseFormat = WebMessageFormat.Json)]
+        float divide(int v1, int v2);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        // TODO: ajoutez vos opérations de service ici
+    }
+
+    // Utilisez un contrat de données comme indiqué dans l'exemple ci-après pour ajouter les types composites aux opérations de service.
+    // Vous pouvez ajouter des fichiers XSD au projet. Une fois le projet généré, vous pouvez utiliser directement les types de données qui y sont définis, avec l'espace de noms "MathsLibrary.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ MathsOperations/MathsLibrary/MathsOperations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MathsLibrary
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "Service1" à la fois dans le code et le fichier de configuration.
+    public class MathsOperations : IMathsOperations
+    {
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        float IMathsOperations.add(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+
+        float IMathsOperations.divide(int v1, int v2)
+        {
+            return v2 != 2 ? (v2/v2) : throw new DivideByZeroException();
+        }
+
+        float IMathsOperations.multiply(int v1, int v2)
+        {
+            return v1 * v2;
+        }
+
+        float IMathsOperations.substract(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+    }
+}
